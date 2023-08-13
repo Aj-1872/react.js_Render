9aa60e9ce6d7a66ee2f51b8897e4358bfcc1cf2a
Aj-1872 <ajayjodedra@gmail.com>
2023-08-12 19:22:29 -0700

    useState and useReducer

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import UseReducer from "./Component/UseReducer/UseReducer";
+import Second from "./Component/UseState/Second";
+function App() {
+  return (
+    //  <Second/>
+   <div>
+   <UseReducer/>
+   </div>
+  );
+}
+
+export default App;
